85a424f4417fb8ed2cd6b8e89f153d29c6602109
Mohammad Shoumik Raihan <37881170+Z0G@users.noreply.github.com>
2021-05-05 12:13:24 +0600

    Made some changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function Todo({ todo, index, markTodo, removeTodo }) {
     return (
         <div
             className="todo"
-
         >
             <span style={{ textDecoration: todo.isDone ? "line-through" : "" }}>{todo.text}</span>
             <div>
@@ -35,7 +34,7 @@ function FormTodo({ addTodo }) {
                 <Form.Label><b>Add Todo</b></Form.Label>
                 <Form.Control type="text" className="input" value={value} onChange={e => setValue(e.target.value)} placeholder="Add new todo" />
             </Form.Group>
-            <Button variant="primary mb-3" type="submit">
+            <Button variant="success mb-3" type="submit">
                 Submit
             </Button>
         </Form>
